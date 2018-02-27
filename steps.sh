4d320b8fee5ca9b9f03715c4c63b0a6ff893b2cc
tovak <pythonpsql@gmail.com>
2018-02-27 09:06:28 +0530

    minor change

--- steps.sh
+++ steps.sh
@@ -3,3 +3,5 @@ git init
 git remote add origin https://github.com/pythonpsql/chip.git
 git pull origin master
 # copy custom_data and fonts folder
+# change terminator default folder
+# correct bash_aliases
