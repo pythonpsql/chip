78a6b44254806ec2a86891f789a4e8a040287c6f
tovak <pythonpsql@gmail.com>
2018-02-27 08:55:34 +0530

    disabled

--- git_push.sh
+++ git_push.sh
@@ -1,3 +1,4 @@
-git add .
-git commit -m "first commit"
-git push -u origin master
+echo 'Currently Disabled. Please do manual commits for a better git'
+# git add .
+# git commit -m "first commit"
+# git push -u origin master
