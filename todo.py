5d1ffacdb0715651f6be729adc927dd23447e80e
pythonpsql@gmail.com <pythonpsql@gmail.com>
2018-03-07 19:01:58 +0530

    added

--- todo.py
+++ todo.py
@@ -1,9 +1,11 @@
 '''
 Features required:
+    gst ledger - gst_opening_balance column does not exist error, probably missing in gst_sale_ledger_view
     correct cost in product table and si_detail
     cost of Barrel Nipple, Extension, GI and pricelist items
     delete temp_vendor product when deleting master
     fix empty gst_rate bug when entering invoice_detail
     shell script to append letters to a file
+
 
 '''
