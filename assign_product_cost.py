376fa9a829d8980c4bebd10838f7be5095ef1581
tovak <pythonpsql@gmail.com>
2018-04-16 07:00:23 +0530

    minor

--- assign_product_cost.py
+++ assign_product_cost.py
@@ -16,6 +16,13 @@ from decimal import Decimal
 
 Database.initialise(database='chip', host='localhost', user='dba_tovak')
 
+def get_a_product(product_name):
+    with conn() as cursor:
+        sq = "select id, name, cost from product where name = %s"
+        cursor.execute(sq, (product_name,))
+        result = cursor.fetchone()
+    return result
+
 def get_all_products():
     with conn() as cursor:
         sq = "select id, name, cost from product"
@@ -25,23 +32,35 @@ def get_all_products():
     # cf.pretty_(['id', 'name', 'cost'], result)
 
     # print(result)
+def set_product_cost(a):
+    cost_before_discount = cf.prompt_("Enter cost for {}: ".format(a[1]), [], default_=str(a[2]), empty_="yes")
+    if cost_before_discount == "None":
+        cost_before_discount = None
+    if cost_before_discount:
+        discount = cf.prompt_("Enter discount for {}: ".format(a[1]), [], default_=str(0), empty_="yes")
+        if discount:
+            discount = float(discount)
+            cost = (Decimal(cost_before_discount) * Decimal(1 - discount/100)).quantize(Decimal("1.00"))
+        else:
+            cost = cost_before_discount
+        timestamp_ = cf.get_current_timestamp()
+        with conn() as cursor:
+            cursor.execute("update product set (cost, timestamp_) = (%s, %s) where id = %s returning name, cost, timestamp_", (cost, timestamp_, a[0]))
+            result = cursor.fetchall()
+        cf.pretty_(['name', 'cost', 'timestamp_'], result)
 
 # product.get_buy_rate(invoice_detail_.product_name)
 if __name__ == "__main__":
-    result = get_all_products()
-    for a in result:
-        product.get_buy_rate(a[1])
-        cost_before_discount = cf.prompt_("Enter cost for {}: ".format(a[1]), [], default_=str(a[2]), empty_="yes")
-        if cost_before_discount == "None":
-            cost_before_discount = None
-        if cost_before_discount:
-            discount = cf.prompt_("Enter discount for {}: ".format(a[1]), [], default_=str(0), empty_="yes")
-            if discount:
-                discount = float(discount)
-                cost = (Decimal(cost_before_discount) * Decimal(1 - discount/100)).quantize(Decimal("1.00"))
-            else:
-                cost = cost_before_discount
-            with conn() as cursor:
-                cursor.execute("update product set cost = %s where id = %s returning name, cost", (cost, a[0]))
-                result = cursor.fetchall()
-            cf.pretty_(['name', 'cost'], result)
+    while True:
+        product_name = cf.prompt_("Choose Product or 'a' for all: ", cf.get_completer_list("name", "product"))
+        if product == "a":
+            result = get_all_products()
+            for a in result:
+                product.get_buy_rate(a[1])
+                set_product_cost(a)
+        else:
+            a = get_a_product(product_name)
+            product.get_buy_rate(product_name)
+            set_product_cost(a)
+
+
